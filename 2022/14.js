6c209e185a644c0709dfb32acfdac9c17494109d
Jordan Ching <ching.jordan@gmail.com>
2022-12-14 01:06:59 -0700

    Day 14
    Added a Grid class util too

---
+++ 2022/14.js
@@ -0,0 +1,192 @@
+import { printResult } from '../utils/PrettyPrint.js';
+import PerformanceTimer from '../utils/PerformanceTimer.js';
+import { getLinesFromInput } from '../utils/Input.js';
+import Grid from '../utils/Grid.js';
+
+
+////////////
+// Part 1 //
+////////////
+
+
+const WALL = '#';
+const SAND = 'o';
+const SAND_ORIGIN = '+';
+
+const SAND_ORIGIN_COORDINATES = [500, 0];
+
+// Looks below a given point for the first non-undefined cell
+function scanBelow(grid, [ originX, originY ]) {
+    let x = originX;
+
+    for(let y = originY; y < grid.height; y++) {
+        const nextY = y + 1;
+
+        if(nextY === grid.height) {
+            return null;
+        }
+
+        if(grid.getCell([x, nextY]) === undefined) {
+            continue;
+        }
+
+        if(grid.getCell([x - 1, nextY]) === undefined) {
+            x--;
+            continue;
+        }
+
+        if(grid.getCell([x + 1, nextY]) === undefined) {
+            x++;
+            continue;
+        }
+
+        // If all cells below us are non-undefined, we've come to rest
+        return [x, y];
+    }
+}
+
+
+export async function puzzle1(input) {
+    const timer = new PerformanceTimer('Puzzle 1');
+
+    const minDim = [ Infinity, 0 ]
+    const maxDim = [ -Infinity, -Infinity ];
+
+    const layout = getLinesFromInput(input).map(line => {
+        return line.split(' -> ').map(x => x.split(',').map((x, i) => {
+            const coord = parseInt(x);
+
+            if(coord < minDim[i]) {
+                minDim[i] = coord;
+            }
+
+            if(coord > maxDim[i]) {
+                maxDim[i] = coord;
+            }
+
+            return coord;
+        }));
+    });
+
+    minDim[0] -= 5;
+    maxDim[0] += 5;
+
+    const width = maxDim[0] - minDim[0] + 1;
+    const height = maxDim[1] - minDim[1] + 1;
+    const gridOptions = {
+        offsetX: minDim[0]
+    };
+
+    const grid = new Grid(width, height, undefined, gridOptions);
+
+    grid.setCell(SAND_ORIGIN_COORDINATES, SAND_ORIGIN);
+
+    layout.forEach(wall => {
+        for(let i = 0; i < wall.length - 1; i++) {
+            const from = wall[i];
+            const to = wall[i + 1];
+
+            grid.setRange(from, to, WALL);
+        }
+    });
+
+    let sandCount = 0;
+
+    let minX = Infinity;
+    let maxX = -Infinity;
+
+    while(true) {
+        const sandRestingCoordinates = scanBelow(grid, [...SAND_ORIGIN_COORDINATES]);
+        
+        if(sandRestingCoordinates === null) {
+            break;
+        }
+
+        maxX = Math.max(sandRestingCoordinates[0], maxX);
+        minX = Math.min(sandRestingCoordinates[0], minX);
+
+        grid.setCell(sandRestingCoordinates, SAND);
+
+        sandCount++;
+    }
+
+    timer.stop();
+
+    printResult(`Part 1 Result`, sandCount, timer, `Sand is resting between ${minX} and ${maxX}`);
+}
+
+
+////////////
+// Part 2 //
+////////////
+
+
+export async function puzzle2(input) {
+    const timer = new PerformanceTimer('Puzzle 2');
+
+    const minDim = [ Infinity, 0 ]
+    const maxDim = [ -Infinity, -Infinity ];
+
+    const layout = getLinesFromInput(input).map(line => {
+        return line.split(' -> ').map(x => x.split(',').map((x, i) => {
+            const coord = parseInt(x);
+
+            if(coord < minDim[i]) {
+                minDim[i] = coord;
+            }
+
+            if(coord > maxDim[i]) {
+                maxDim[i] = coord;
+            }
+
+            return coord;
+        }));
+    });
+
+    minDim[0] -= 150;
+    maxDim[0] += 150;
+
+    maxDim[1] += 2;
+
+    layout.push([[minDim[0], maxDim[1]], [maxDim[0], maxDim[1]]]);
+
+    const width = maxDim[0] - minDim[0] + 1;
+    const height = maxDim[1] - minDim[1] + 1;
+    const gridOptions = {
+        offsetX: minDim[0]
+    };
+
+    const grid = new Grid(width, height, undefined, gridOptions);
+
+    grid.setCell(SAND_ORIGIN_COORDINATES, SAND_ORIGIN);
+
+    layout.forEach(wall => {
+        for(let i = 0; i < wall.length - 1; i++) {
+            grid.setRange(wall[i], wall[i + 1], WALL);
+        }
+    });
+
+    let sandCount = 0;
+
+    let minX = Infinity;
+    let maxX = -Infinity;
+
+    while(true) {
+        const sandRestingCoordinates = scanBelow(grid, [...SAND_ORIGIN_COORDINATES]);
+
+        maxX = Math.max(sandRestingCoordinates[0], maxX);
+        minX = Math.min(sandRestingCoordinates[0], minX);
+
+        grid.setCell(sandRestingCoordinates, SAND);
+
+        sandCount++;
+
+        if(Grid.AreCoordinatesEqual(sandRestingCoordinates, SAND_ORIGIN_COORDINATES)) {
+            break;
+        }
+    }
+
+    timer.stop();
+
+    printResult(`Part 2 Result`, sandCount, timer, `Sand is resting between ${minX} and ${maxX}`);
+}
---
+++ utils/Grid.js
@@ -0,0 +1,95 @@
+export default class Grid {
+    constructor(width, height, initialValue, options) {
+        this.width = width;
+        this.height = height;
+
+        this.offsetX = options?.offsetX ?? 0;
+        this.offsetY = options?.offsetY ?? 0;
+
+        this.validDimensions = `X => [0,${this.width + this.offsetX}] | Y => [0,${this.height + this.offsetY}]`;
+
+        this.grid = new Array(height)
+            .fill(undefined)
+            .map(() => new Array(width).fill(typeof initialValue === 'function' ? initialValue() : initialValue));
+    }
+
+    getOffsetCoordinates(coordinates) {
+        return [
+            coordinates[0] - this.offsetX,
+            coordinates[1] - this.offsetY
+        ];
+    }
+
+    isValidCell(coordinates) {
+        const [x, y] = this.getOffsetCoordinates(coordinates);
+
+        return x >= 0 && x < this.width && y >= 0 && y < this.height;
+    }
+
+    getCell(coordinates) {
+        if(!this.isValidCell(coordinates)) {
+            return null;
+        }
+
+        const [x, y] = this.getOffsetCoordinates(coordinates);
+
+        return this.grid[y][x];
+    }
+
+    setCell(coordinates, value) {
+        if(!this.isValidCell(coordinates)) {
+            throw new Error(`Cell coordinates [${coordinates.join(', ')}] are invalid! ${this.validDimensions}`);
+        }
+
+        const [x, y] = this.getOffsetCoordinates(coordinates);
+
+        this.grid[y][x] = value;
+    }
+
+    // Range coordinates are inclusive!
+    setRange(from, to, value) {
+        if(!this.isValidCell(from)) {
+            throw new Error(`"from" coordinates [${from.join(', ')}] are invalid! ${this.validDimensions}`);
+        }
+
+        if(!this.isValidCell(to)) {
+            throw new Error(`"to" coordinates [${to.join(', ')}] are invalid! ${this.validDimensions}`);
+        }
+
+        const startX = Math.min(from[0], to[0]);
+        const startY = Math.min(from[1], to[1]);
+        const endX = Math.max(from[0], to[0]);
+        const endY = Math.max(from[1], to[1]);
+
+        for(let x = startX; x <= endX; x++) {
+            for(let y = startY; y <= endY; y++) {
+                const [_x, _y] = this.getOffsetCoordinates([x, y]);
+
+                this.grid[_y][_x] = typeof value === 'function' ? value([x, y]) : value;
+            }
+        }
+    }
+
+    getRow(y) {
+        const [, offsetY] = this.getOffsetCoordinates(coordinates);
+        return this.grid[offsetY];
+    }
+
+    getColumn(x) {
+        const [offsetX] = this.getOffsetCoordinates(coordinates);
+        return this.grid.map(row => row[offsetX]);
+    }
+
+    print(emptyCellValue = '.') {
+        const padLength = `${this.height + this.offsetY}`.length;
+        this.grid.forEach((row, i) => console.log(i.toString().padEnd(padLength + 1), row.map(cell => cell ?? emptyCellValue).join('')));
+    }
+
+    static GetManhattanDistance(start, end) {
+        return Math.abs(start[0] - end[0]) + Math.abs(start[1] - end[1]);
+    }
+
+    static AreCoordinatesEqual(coord1, coord2) {
+        return coord1[0] === coord2[0] && coord1[1] === coord2[1];
+    }
+}
