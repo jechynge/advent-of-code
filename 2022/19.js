aef046820c99806683046a432db627fbbf941b03
Jordan Ching <ching.jordan@gmail.com>
2022-12-27 18:58:24 -0700

    Day 19 done

--- 2022/19.js
+++ 2022/19.js
@@ -62,26 +62,6 @@ class Robots {
         }, {});
     }
 
-    getTimeToBuild(currentResources, resourcesPerTurn) {
-        return ROBOT_TYPES.reduce((accum, robotType) => {
-            let timeNeeded = 0;
-
-            for(const [resourceType, cost] of Object.entries(this.robotCosts[robotType])) {
-                if(resourcesPerTurn[resourceType] === 0) {
-                    timeNeeded = -1;
-                    break;
-                }
-
-                timeNeeded = Math.max(timeNeeded, Math.ceil((cost - currentResources[resourceType]) / resourcesPerTurn[resourceType]), 0);
-            }
-
-            return {
-                ...accum,
-                [robotType]: timeNeeded
-            }
-        }, {});
-    }
-
     build(currentResources, robotType) {
         return Object.entries(currentResources).reduce((accum, [ resourceType, amount ]) => {
             accum[resourceType] = amount - (this.robotCosts[robotType][resourceType] ?? 0);
@@ -90,99 +70,88 @@ class Robots {
         }, {});
     }
 
-    geodeHeuristic(currentGeodeCount, minutesRemaining) {
-        return currentGeodeCount + ((minutesRemaining - 1) / 2) * (minutesRemaining);
+    /*
+     * Assuming the best case scenario is that we can build 1 geode-cracking bot every minute until time expires, how many geodes would we have
+     *
+     * If minutesRemaining = 6, for example:
+     * 
+     * Minute 6: Build 1 geode bot, crack 0 geodes, have  0 geodes
+     * Minute 5: Build 1 geode bot, crack 1 geodes, have  1 geodes
+     * Minute 4: Build 1 geode bot, crack 2 geodes, have  3 geodes
+     * Minute 3: Build 1 geode bot, crack 3 geodes, have  6 geodes
+     * Minute 2: Build 1 geode bot, crack 4 geodes, have 10 geodes
+     * Minute 1: Build 1 geode bot, crack 5 geodes, have 15 geodes
+     */
+    geodeHeuristic(currentGeodeCount, geodeRobotCount, minutesRemaining) {
+        return currentGeodeCount + (geodeRobotCount * minutesRemaining) + ((minutesRemaining - 1) / 2) * (minutesRemaining);
     }
 
     findBestBuildOrder() {
-        let checkedBuilds = 0;
         let mostGeodes = 0;
-        let totalBuilt = {};
 
         const maxOreRobotsWanted = Math.max(this.robotCosts.clay.ore, this.robotCosts.obsidian.ore, this.robotCosts.geode.ore);
 
-        console.log(`Building up to ${maxOreRobotsWanted} ore, ${this.robotCosts.obsidian.clay} clay, and ${this.robotCosts.geode.obsidian} obsidian`);
-
-        const evaluateNextBuildStep = (currentResources, robots, minutesRemaining, waitingToPurchase = {
-            ore: true,
-            clay: true,
-            obsidian: true,
-            geode: true
-        }) => {
+        const evaluateNextBuildStep = (currentResources, robots, minutesRemaining, skippedBuilds) => {
             // If we're out of time, check our result against the best and bail out
             if(minutesRemaining === 0) {
-                checkedBuilds++;
-
                 if(currentResources.geode > mostGeodes) {
                     mostGeodes = currentResources.geode;
-                    totalBuilt = {...robots};
                 }
 
                 return;
             }
 
             // If we can't beat the current best score assuming we could build another geode robot per turn, bail out of this branch
-            if(this.geodeHeuristic(currentResources.geode, minutesRemaining) <= mostGeodes) {
+            if(this.geodeHeuristic(currentResources.geode, robots.geode, minutesRemaining) < mostGeodes) {
                 return;
             }
+
+            const possibleBuilds = [];
 
             const affordableRobots = this.affordableRobots(currentResources);
-            const potentialNextBuilds = { ...waitingToPurchase };
-
-            // If I can't afford anything, move on to next step
-            if(Object.values(affordableRobots).every(canAfford => !canAfford) === 0) {
-                evaluateNextBuildStep(this.addResources(robots, currentResources), [...robots], minutesRemaining - 1);
-                return;
-            }
+            const nextResources = this.addResources(robots, currentResources);
 
             // If I can afford a geode-cracking robot, build it, and move on
             if(affordableRobots.geode) {
-                const nextResources = this.addResources(robots, this.build(currentResources, 'geode'));
                 const nextRobots = {
                     ...robots,
                     geode: robots.geode + 1
                 };
 
-                evaluateNextBuildStep(nextResources, nextRobots, minutesRemaining - 1);
-                return;
+                return evaluateNextBuildStep(this.build(nextResources, 'geode'), nextRobots, minutesRemaining - 1);
             }
 
-            let possibleBuilds = [];
 
             // If I can afford an geode-cracking bot...
-            // if(affordableRobots.geode) {
-            //     possibleBuilds.push('geode');
-            // }
+            if(affordableRobots.geode) {
+                possibleBuilds.push('geode');
+            }
             
             // If I can afford an obsidian-gathering bot...
-            if(affordableRobots.obsidian && robots.obsidian < this.robotCosts.geode.obsidian && waitingToPurchase.obsidian) {
+            if(affordableRobots.obsidian && robots.obsidian < this.robotCosts.geode.obsidian && !skippedBuilds?.obsidian) {
                 possibleBuilds.push('obsidian');
             }
             
-            if(affordableRobots.clay && robots.clay < this.robotCosts.obsidian.clay && waitingToPurchase.clay) {
+            if(affordableRobots.clay && robots.clay < this.robotCosts.obsidian.clay && !skippedBuilds?.clay) {
                 possibleBuilds.push('clay');
             }
 
             // If I can afford an ore-drilling robot, and I only have the inital one, and I only have a few robots, try building one
-            if(affordableRobots.ore && robots.ore < maxOreRobotsWanted && waitingToPurchase.ore) {
+            if(affordableRobots.ore && robots.ore < maxOreRobotsWanted && !skippedBuilds?.ore) {
                 possibleBuilds.push('ore');
             }
 
             // Try building each potential robot we can afford
             possibleBuilds.forEach(robotType => {
-                const nextResources = this.addResources(robots, this.build(currentResources, robotType));
                 const nextRobots = {
                     ...robots,
                     [robotType]: robots[robotType] + 1
                 };
 
-                evaluateNextBuildStep(nextResources, nextRobots, minutesRemaining - 1);
+                evaluateNextBuildStep(this.build(nextResources, robotType), nextRobots, minutesRemaining - 1);
             });
 
-            if(possibleBuilds.length > 0) {
-
-            }
-            evaluateNextBuildStep(this.addResources(robots, currentResources), {...robots}, minutesRemaining - 1);
+            evaluateNextBuildStep(nextResources, {...robots}, minutesRemaining - 1, {...affordableRobots});
         }
 
         const startingResources = {
@@ -201,16 +170,7 @@ class Robots {
 
         evaluateNextBuildStep(startingResources, startingRobots, this.totalMinutes);
 
-        console.log(`produced ${mostGeodes} geodes - checked ${checkedBuilds} builds. Total robots built:`);
-        Object.entries(totalBuilt).forEach(([ robotType, amount ]) => {
-            console.log(`${robotType}: ${amount}`);
-        })
-
         return mostGeodes;
-    }
-
-    timeRemaining() {
-        return this.totalMinutes - this.elapsedMinutes;
     }
 }
 
@@ -239,9 +199,15 @@ export async function puzzle1(input) {
 export async function puzzle2(input) {
     const timer = new PerformanceTimer('Puzzle 2');
 
-    // ...todo
+    const blueprints = getLinesFromInput(input).slice(0,3).map(blueprint => new Robots(blueprint, 32));
+
+    const qualityProduct = blueprints.reduce((product, blueprint) => {
+        const quality = blueprint.findBestBuildOrder();
+
+        return quality * product;
+    }, 1);
 
     timer.stop();
 
-    printResult(`Part 2 Result`, null, timer);
+    printResult(`Part 2 Result`, qualityProduct, timer);
 }
