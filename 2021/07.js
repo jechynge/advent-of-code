d93f7b12e3a4861bcebf64026ea26495dfe9402d
Jordan Ching <ching.jordan@gmail.com>
2023-10-29 16:00:55 -0600

    Day 7

---
+++ 2021/07.js
@@ -0,0 +1,68 @@
+import { printResult } from '../utils/PrettyPrint.js';
+import PerformanceTimer from '../utils/PerformanceTimer.js';
+import { getLinesFromInput } from '../utils/Input.js';
+
+////////////
+// Part 1 //
+////////////
+
+
+export async function puzzle1(input) {
+    const timer = new PerformanceTimer('Puzzle 1');
+
+    const positions = input.split(',').map(position => parseInt(position)).sort((a, b) => a - b);
+
+    const modes = [Math.floor(positions.length / 2)];
+
+    if(positions.length % 2 === 0) {
+        modes.push(Math.ceil(positions.length / 2));
+    }
+
+    const calculateMoveToIndex = (positions, targetPosition) => {
+        return positions.reduce((totalCost, position) => {
+            return Math.abs(position - targetPosition) + totalCost;
+        }, 0);
+    };
+
+    const costs = modes.map(modeIndex => calculateMoveToIndex(positions, positions[modeIndex]));
+
+    const lowestCost = Math.min(...costs);
+
+    timer.stop();
+
+    printResult(`Part 1 Result`, lowestCost, timer);
+}
+
+
+////////////
+// Part 2 //
+////////////
+
+
+export async function puzzle2(input) {
+    const timer = new PerformanceTimer('Puzzle 2');
+
+    const positions = input.split(',').map(position => parseInt(position)).sort((a, b) => a - b);
+
+    const average = positions.reduce((sum, position) => sum + position, 0) / positions.length;
+
+    const averageIndices = [Math.ceil(average), Math.floor(average)];
+
+    const sumInts = x => {
+        return x * ( x + 1 ) / 2;
+    }
+    
+    const calculateMoveToIndex = (positions, targetPosition) => {
+        return positions.reduce((totalCost, position) => {
+            return sumInts(Math.abs(position - targetPosition)) + totalCost;
+        }, 0);
+    }
+
+    const costs = averageIndices.map(averageIndex => calculateMoveToIndex(positions, averageIndex));
+
+    const lowestCost = Math.min(...costs);
+
+    timer.stop();
+
+    printResult(`Part 2 Result`, lowestCost, timer);
+}
