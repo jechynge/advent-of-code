71b787c2194082271ddba8c7d7bb5750604a750c
Jordan Ching <jching@addictinggames.com>
2023-12-12 18:01:17 -0700

    main utils

--- utils/Math.js
+++ utils/Math.js
@@ -5,3 +5,5 @@ export const lowestCommonMultiple = (a, b) => a * b / greatestCommonDivisor(a, b
 export const sum = (a, b) => a + b;
 
 export const product = (a, b) => a * b;
+
+export const percent = (numerator, denominator, max = Infinity) => Math.min(max, denominator > 0 ? Math.floor(numerator / denominator * 100) : 0);
---
+++ utils/ProgressMeter.js
@@ -0,0 +1,64 @@
+import { percent } from './Math';
+import _ from 'lodash';
+
+const SPINNER_CHARACTERS = ['|', '/', '-', '\\'];
+
+const SPINNER_INTERVAL = 250;
+
+class ProgressMeter {
+
+  constructor(total, options) {
+
+    options = {
+      showSpinner: true
+    };
+
+    this.total = total;
+
+    this.progress = 0;
+
+    this.percentProgress = 0;
+
+    Object.entries(options).forEach(([option, value]) => {
+      this[option] = value;
+    });
+
+    if(this.showSpinner) {
+      this.spinnerInterval = setInterval(() => this.print(), SPINNER_INTERVAL);
+    }
+
+  }
+
+  tickProgress() {
+    this.addProgress(); 
+  }
+
+  addProgress(progress = 1) {
+    this._setProgress(this.progress + progress);
+  }
+
+  _setProgress(progress, silent = false) {
+    this.progress = Math.min(progress, this.total);
+    const prevPercentProgress = this.percentProgress;
+    this.percentProgress = percent(this.progress, this.total);
+
+    if(!silent && this.percentProgress > prevPercentProgress) {
+      this.print();
+    }
+
+    if(this.progress === this.total) {
+      clearInterval(this.spinnerInterval);
+    }
+  }
+
+  print() {
+    _.debounce(this._print(), 300, {
+      trailing: true
+    });
+  }
+
+  _print() {
+
+  }
+
+}
