a349572390384be60e35a26098ae81431113b1b4
Jordan Ching <ching.jordan@gmail.com>
2023-12-17 01:55:16 -0700

    2023 Day 17 Part 1

--- 2023/17.js
+++ 2023/17.js
@@ -5,19 +5,24 @@ import CloneDeep from 'lodash.clonedeep';
 
 const MAX_MOMENTUM = 3;
 
-const Step = (from, to, directionIndex, momentum, cost) => ({
-    from,
-    to,
+const DIRECTION_SYMBOLS = ['^', '>', 'v', '<'];
+
+const Step = (prevCoord, currentCoord, directionIndex, momentum, knownCost, estimatedCost) => ({
+    prevCoord,
+    currentCoord,
     directionIndex,
     momentum,
-    cost
+    knownCost,
+    estimatedCost
 });
 
 const cellInfo = new Array(GRID_CARDINAL_MOVEMENT_NAMES.length).fill(null).map(() => {
     return new Array(MAX_MOMENTUM).fill(null).map(() => ({
         knownCost: Infinity,
         estimatedCost: Infinity,
-        previousCoordinates: null
+        previousCoordinates: null,
+        previousDirectionIndex: null,
+        previousMomentum: null
     }));
 });
 
@@ -25,6 +30,7 @@ const generateCellInfo = (costString) => {
     const cell = CloneDeep(cellInfo);
 
     cell.cost = parseInt(costString);
+    cell.symbol = null;
 
     return cell;
 }
@@ -42,12 +48,15 @@ export async function firstPuzzle(input) {
     const START_COORDINATES = [0, 0];
     const DESTINATION_COORDINATES = [ grid.width - 1, grid.height - 1 ];
 
-    const steps = [Step(null, START_COORDINATES, null, 0, Grid.GetManhattanDistance(START_COORDINATES, DESTINATION_COORDINATES))];
+    const steps = [Step(null, START_COORDINATES, null, 0, 0, Grid.GetManhattanDistance(START_COORDINATES, DESTINATION_COORDINATES))];
+
+    let lastStep;
 
     while(steps.length > 0) {
         const step = steps.shift();
 
-        if(Grid.AreCoordinatesEqual(step.to, DESTINATION_COORDINATES)) {
+        if(Grid.AreCoordinatesEqual(step.currentCoord, DESTINATION_COORDINATES)) {
+            lastStep = step;
             break;
         }
 
@@ -63,25 +72,71 @@ export async function firstPuzzle(input) {
                 continue;
             }
 
-            const to = Grid.Transform2DCoordinate(step.to, GRID_CARDINAL_TRANSFORMS[i]);
+            const nextCoord = Grid.Transform2DCoordinate(step.currentCoord, GRID_CARDINAL_TRANSFORMS[i]);
 
-            const cell = grid.getCell(to);
+            const nextCell = grid.getCell(nextCoord);
 
             // If the next coordinates aren't in the grid
-            if(cell === null) {
+            if(nextCell === null) {
                 continue;
             }
 
-            const cost = step.cost + cell.cost;
-            const estimatedCost = Grid.GetManhattanDistance(to, DESTINATION_COORDINATES);
+            const momentum = step.directionIndex === i ? step.momentum + 1 : 1;
 
-            
+            const knownCost = step.knownCost + nextCell.cost;
+            const estimatedCost = Grid.GetManhattanDistance(nextCoord, DESTINATION_COORDINATES);
+
+            const currentBestInfo = nextCell[i][momentum - 1];
+
+            // If we've visited this cell from this direction and with the
+            // same momentum and this path isn't better, continue.
+            if(currentBestInfo.knownCost + currentBestInfo.estimatedCost <= knownCost + estimatedCost) {
+                continue;
+            }
+
+            currentBestInfo.knownCost = knownCost;
+            currentBestInfo.estimatedCost = estimatedCost;
+            currentBestInfo.previousCoordinates = [...step.currentCoord];
+            currentBestInfo.previousMomentum = step.momentum;
+            currentBestInfo.previousDirectionIndex = step.directionIndex;
+
+            const nextStep = Step(step.currentCoord, nextCoord, i, momentum, knownCost, estimatedCost);
+
+            const insertAt = steps.findIndex((step) => knownCost + estimatedCost < step.knownCost + step.estimatedCost);
+
+            if(insertAt === -1) {
+                steps.push(nextStep);
+            } else {
+                steps.splice(insertAt, 0, nextStep);
+            }
         }
     }
 
+    // let coord = lastStep.currentCoord;
+    // let directionIndex = lastStep.directionIndex;
+    // let momentum = lastStep.momentum;
 
+    // while(coord) {
+    //     const c = grid.getCell(coord);
 
-    return { answer: null, extraInfo: undefined };
+    //     c.symbol = DIRECTION_SYMBOLS[directionIndex];
+
+    //     const prevCellInfo = c?.[directionIndex]?.[momentum - 1];
+
+    //     if(!prevCellInfo) {
+    //         break;
+    //     }
+
+    //     coord = prevCellInfo.previousCoordinates;
+    //     directionIndex = prevCellInfo.previousDirectionIndex;
+    //     momentum = prevCellInfo.previousMomentum;
+    // }
+
+    // grid.print({
+    //     mapValue: x => x.symbol ?? ' '
+    // });
+
+    return { answer: lastStep.knownCost, extraInfo: undefined };
 
 }
 
