ffd7b43e2e6c98673557ecaa2b212094a3708265
Jordan Ching <ching.jordan@gmail.com>
2023-12-29 00:24:03 -0700

    Update solutionTemplate.js

--- solutionTemplate.js
+++ solutionTemplate.js
@@ -1,5 +1,5 @@
 import { getLinesFromInput } from '../utils/Input.js';
-
+import { Grid, constructGridFromInput } from '../utils/Grid.js';
 
 ////////////
 // Part 1 //
@@ -25,5 +25,33 @@ export async function secondPuzzle(input) {
     // ...todo
 
     return { answer: null, extraInfo: undefined };
+
+}
+
+
+////////////
+// Tests  //
+////////////
+
+
+export async function test(input) {
+
+    const tests = [
+        
+    ];
+
+    const failed = [];
+
+    for(let i = 0; i < tests.length; i++) {
+        const [ expected, input, ...params ] = tests[ i ];
+
+        const { answer } = await secondPuzzle(input, ...params);
+
+        if(answer !== expected) {
+            failed.push(`Test ${i}: expected ${expected} - got ${answer}`);
+        }
+    }
+
+    return { passed: failed.length === 0, extraInfo: failed.length === 0 ? `Passed ${tests.length} tests` : `Failed ${failed.length} tests:\n${failed.join('\n')}` };
 
 }
